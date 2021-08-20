525593508b7e165a616e8cd272b9159b5dd943dc
y1220 <yui.watanabe@mail.polimi.it>
2021-08-20 23:01:25 +0200

    divide into train, test sets, predict based on them

--- logistic regression model.R
+++ logistic regression model.R
@@ -15,3 +15,23 @@ xtabs(~admit + rank, data= mydata)
 
 mylogit = glm(admit~gre+gpa+rank, data=mydata, family='binomial')
 summary(mylogit)
+
+# divide the dataset into training and test for analysis
+n= nrow(mydata)
+sample= sample(1:n, size= round(0.7*n), replace = FALSE)
+train= mydata[sample,]
+test= mydata[-sample,]
+
+# again build the logistic regression model
+logR1= glm(admit~gre+gpa+rank, train, family='binomial')
+logR1
+summary(logR1)
+
+# predict on test set
+predicted= predict(logR1, newdata= test, type= 'response')
+head(predicted)
+predictV= factor(ifelse(predicted<0.5, 0, 1))
+head(predictV)
+test= cbind(test, predictV)
+head(test)
+str(test)
