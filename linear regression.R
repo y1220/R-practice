36117cca6e04a815ca07195ec94c4c4821ac8d49
y1220 <yui.watanabe@mail.polimi.it>
2021-08-20 18:11:29 +0200

    regression line, prediction on x values

--- linear regression.R
+++ linear regression.R
@@ -8,3 +8,18 @@ plot(women$height, women$weight)
 lmodel = lm(weight~height, data=women)
 lmodel
 summary(lmodel)
+
+# regression line
+abline(lm(weight~height, data=women), col='red', lwd=1)
+
+# predict the value of y(weight) with a new data set of x(height)
+head(women)
+range(women$weight)
+range(women$height)
+newdata= data.frame(height=c(50,75,80,85))
+newdata
+pred_weight= predict(lmodel, newdata)
+pred_weight
+
+# check assumption of the model by dignostic plotting
+plot(lmodel)
