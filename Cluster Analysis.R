f4e2a5d74ec8a6f12224b3a866b56d0c567a72bc
y1220 <yui.watanabe@mail.polimi.it>
2021-09-09 12:45:04 +0200

    kmeans, scaling, cbind to remake matrix

--- Cluster Analysis.R
+++ Cluster Analysis.R
@@ -41,3 +41,55 @@ rect.hclust(hc.average, k= 4, border= 3:7)
 plot(hc.average, hang= -1, cex= .8, main= 'Average Linkage Clustering')
 rect.hclust(hc.average, h=100)
 abline(h=100)
+
+# clustering with mtcars
+pacman::p_load(dplyr, ggplot2)
+head(mtcars)
+df= mtcars[,c('mpg','wt','hp', 'am')]
+head(df)
+
+# kmeans 
+# building kmeans clustering models with 3 centers
+km1= kmeans(df, centers=3)
+km1
+cbind(df,km1$cluster)
+table(km1$cluster) # help to know the number of observation in each cluster
+df[km1$cluster==1,]
+df %>% filter(km1$cluster==2)
+df %>% arrange(km1$cluster)
+
+# based on clusters lets find the mean of variables in our dataset
+df %>% group_by(clusterNo= km1$cluster) %>% summarise_all(mean, na.rm=T)
+
+# plotting the clusters using package factoextra
+library(factoextra)
+fviz_cluster(km1, data=df)
+
+# factoextra method
+set.seed(123)
+fviz_nbclust(df, kmeans, method= 'wss') # with-in-of-squares
+fviz_nbclust(df, kmeans, method= 'wss') + geom_vline(xintercept= 3, linetype= 2) +
+  labs(subtitle= 'Elbow method')
+# xintercept: Parameters that control the position of the line
+# from above function(elbow method) we get to know, the best no. of cluster is 2,3, or 4
+
+# Verify with NbClust method:
+library(NbClust)
+set.seed(1234)
+nc <- NbClust(df, distance = 'euclidean', min.nc=2, max.nc=6, method= 'kmeans') 
+nc
+
+# Do scale since the value at marks and project differs
+# 5 students into 2 groups
+marks1= c(65, 60, 70, 75, 67)
+marks2= c(95, 80, 52, 55, 57)
+project= c(120, 100, 90, 80, 110)
+(exam3= data.frame(marks1, marks2, project))
+# project marks will be more dominating
+exam3scaled= scale(exam3)
+exam3scaled # -3 to 3 : Z score scaling
+
+cluster2C= kmeans(exam3scaled, centers=2)
+cluster2C
+cluster2C$cluster
+cbind(exam3, group=cluster2C$cluster)
