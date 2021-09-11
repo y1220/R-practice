00c2b6a9dfd89a89f3b462bac453b5749cd87fac
y1220 <yui.watanabe@mail.polimi.it>
2021-09-11 18:16:37 +0200

    slider, top_n for limiting the number of output

--- shiny slider.R
+++ shiny slider.R
@@ -1,13 +1,14 @@
 library(shiny)
 library(ggplot2)
+library(babynames)
+library(tidyverse)
 ui <- fluidPage(
   titlePanel("What's in a Name?"),
-  # CODE BELOW: Add select input named "sex" to choose between "M" and "F"
-  selectInput(
-    'sex', 
-    'Select Sex', 
-    selected = 'F', 
-    choices = c('F', 'M')
+  # Add select input named "sex" to choose between "M" and "F"
+  selectInput('sex', 'Select Sex', choices = c("F", "M")),
+  # CODE BELOW: Add slider input named 'year' to select years  (1900 - 2010)
+  sliderInput(
+    'year', 'select year', value= 1900, min= 1900, max= 2010
   ),
   # Add plot output to display top 10 most popular names
   plotOutput('plot_top_10_names')
@@ -17,10 +18,10 @@ server <- function(input, output, session){
   # Render plot of top 10 most popular names
   output$plot_top_10_names <- renderPlot({
     # Get top 10 names by sex and year
-    top_10_names <- bnames2 %>% 
-      # MODIFY CODE BELOW: Filter for the selected sex
+    top_10_names <- babynames %>% 
       filter(sex == input$sex) %>% 
-      filter(year == 1900) %>% 
+      # MODIFY CODE BELOW: Filter for the selected year
+      filter(year == input$year) %>% 
       top_n(10, prop)
     # Plot top 10 names by sex and year
     ggplot(top_10_names, aes(x = name, y = prop)) +
