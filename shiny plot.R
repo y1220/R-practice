06affd2ba025f633589364543e9d3bdb2fdb1226
y1220 <yui.watanabe@mail.polimi.it>
2021-09-11 17:53:28 +0200

    replace the part of baby into vehicles

--- shiny plot.R
+++ shiny plot.R
@@ -14,12 +14,25 @@ ui <- fluidPage(
   
 )
 
+g <- ggplot(df, aes(Var1, Freq))
+g + geom_bar(stat="identity", width = 0.5, fill="tomato2") + 
+  labs(title="Bar Chart", 
+       subtitle="Manufacturer of vehicles", 
+       caption="Source: Frequency of Manufacturers from 'mpg' dataset") +
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+
 server <- function(input, output, session) {
   output$trend <- renderPlot({
-    ggplot(subset(babynames, name == "David")) +
-      geom_line(aes(x = year, y = prop, color = sex))
+    # ggplot(subset(babynames, name == "David")) +
+    #   geom_line(aes(x = year, y = prop, color = sex))
+    g <- ggplot(df, aes(Var1, Freq))
+    g + geom_bar(stat="identity", width = 0.5, fill="tomato2") + 
+      labs(title="Bar Chart", 
+           subtitle="Manufacturer of vehicles", 
+           caption="Source: Frequency of Manufacturers from 'mpg' dataset") +
+      theme(axis.text.x = element_text(angle=65, vjust=0.6))
 
   })
 }
 shinyApp(ui = ui, server = server)
-?ggplot2
+
