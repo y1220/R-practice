1b6441782bd856715a20c28b5ccd38e5fb358c48
y1220 <yui.watanabe@mail.polimi.it>
2021-09-11 20:08:00 +0200

    output <- plotly::renderPlotly

---
+++ shiny plotly.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(ggplot2)
+library(babynames)
+library(tidyverse)
+library(plotly)
+
+# generate top 20 popuplar names
+tab <- table(a<-babynames$name)
+df <- as.data.frame(tab)
+names(df) <- c("name","times")
+df$rank <- rank(-df$times,ties.method="min")
+df <- df[order(df$rank,decreasing = F),]
+top_trendy_names <- df$name
+ui <- fluidPage(
+  selectInput('name', 'Select Name', top_trendy_names),
+  # selectInput('name', 'Select Name', top_trendy_names$name),
+  # CODE BELOW: Add a plotly output named 'plot_trendy_names'
+  plotly::plotlyOutput('plot_trendy_names')
+)
+server <- function(input, output, session){
+  # Function to plot trends in a name
+  plot_trends <- function(){
+    babynames %>% 
+      filter(name == input$name) %>% 
+      ggplot(aes(x = year, y = n)) +
+      geom_col()
+  }
+  # CODE BELOW: Render a plotly output named 'plot_trendy_names'
+  output$plot_trendy_names <- plotly::renderPlotly({
+    plot_trends()
+  })
+  
+}
+shinyApp(ui = ui, server = server)
