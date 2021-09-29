942001b0376dd373cd8e35405ed122262972ced4
y1220 <yui.watanabe@mail.polimi.it>
2021-09-29 23:01:06 +0200

    for loop, if, else in function with default value

--- function.R
+++ function.R
@@ -24,3 +24,69 @@ hello <- function() {
 
 # Call the function hello()
 hello()
+
+# Finish the pow_two() function
+pow_two <- function(x, print_info= TRUE) {
+  y <- x ^ 2
+  if(print_info == TRUE){
+    print(paste(x, "to the power two equals", y))
+  }
+  
+  return(y)
+}
+
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16,9,13,5,2,17,14)
+facebook <- c(17,7,5,16,8,13,14)
+
+
+# Define the interpret function
+interpret <- function(num_views) {
+  if (num_views > 15) {
+    print("You're popular!")
+    return(num_views)
+  } else {
+    print("Try to be more visible!")
+    return(0)
+  }
+}
+
+# Call the interpret function twice
+interpret(linkedin[1])
+interpret(facebook[2])
+
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# The interpret() can be used inside interpret_all()
+interpret <- function(num_views) {
+  if (num_views > 15) {
+    print("You're popular!")
+    return(num_views)
+  } else {
+    print("Try to be more visible!")
+    return(0)
+  }
+}
+
+# Define the interpret_all() function
+# views: vector with data to interpret
+# return_sum: return total number of views on popular days?
+interpret_all <- function(views, return_sum = TRUE) {
+  count <- 0
+  
+  for (v in views) {
+    count <- count + interpret(v)
+  }
+  
+  if (return_sum == TRUE) {
+    return(count)
+  } else {
+    return(NULL)
+  }
+}
+
+# Call the interpret_all() function on both linkedin and facebook
+interpret_all(linkedin)
+interpret_all(facebook)
