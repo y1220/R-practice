c5cd77ca8ed0c6329b56debe360c01a2f3fca3d2
y1220 <yui.watanabe@mail.polimi.it>
2021-09-29 22:36:47 +0200

    function to compute poower and abs

---
+++ function.R
@@ -0,0 +1,17 @@
+# Create a function pow_two()
+pow_two <- function(arg) {
+  arg*arg
+}
+
+
+# Use the function
+pow_two(12)
+
+# Create a function sum_abs()
+sum_abs <- function(arg1, arg2) {
+  abs(arg1)+ abs(arg2)
+}
+
+
+# Use the function
+sum_abs(-2, 3)
