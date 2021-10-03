9307b8b3cbf5fa6a444fbb42be988206896ced31
y1220 <yui.watanabe@mail.polimi.it>
2021-10-03 18:26:12 +0200

    filter, arrange for sort, mutate to add a column

---
+++ date time.R
@@ -0,0 +1,37 @@
+# Get the current date: today
+today <- Sys.Date()
+
+# See what today looks like under the hood
+unclass(today)
+
+# Get the current time: now
+now <- Sys.time()
+
+# See what now looks like under the hood
+unclass(now)
+
+# %Y: 4-digit year (1982)
+# %y: 2-digit year (82)
+# %m: 2-digit month (01)
+# %d: 2-digit day of the month (13)
+# %A: weekday (Wednesday)
+# %a: abbreviated weekday (Wed)
+# %B: month (January)
+# %b: abbreviated month (Jan)
+
+# Definition of character strings representing dates
+str1 <- "May 23, '96"
+str2 <- "2012-03-15"
+str3 <- "30/January/2006"
+
+# Convert the strings to dates: date1, date2, date3
+date1 <- as.Date(str1, format = "%b %d, '%y")
+date2 <- as.Date(str2)
+date3 <- as.Date(str3, format = "%d/%b/%Y")
+
+
+
+# Convert dates to formatted strings
+format(date1, "%A")
+format(date2, "%d")
+format(date3, "%b %Y")
