8273c4fa327cf6ff19c191dc3cf13890c204dc88
y1220 <yui.watanabe@mail.polimi.it>
2021-10-05 16:35:41 +0200

    scale log10

--- gapminder.R
+++ gapminder.R
@@ -35,3 +35,14 @@ gapminder %>%
   mutate(lifeExpMonths = 12 * lifeExp) %>%
   arrange(desc(lifeExpMonths))
 
+
+library(ggplot2)
+
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+# Scatter plot comparing pop and gdpPercap, with both axes on a log scale
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) +
+  geom_point() +
+  scale_x_log10() + scale_y_log10()
+
