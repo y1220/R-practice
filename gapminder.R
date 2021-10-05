03f5ba2c9c19d359f3d55dc431511323aef745b1
y1220 <yui.watanabe@mail.polimi.it>
2021-10-05 17:09:39 +0200

    group_by, summarize to list up the statistics

--- gapminder.R
+++ gapminder.R
@@ -65,3 +65,38 @@ ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) +
   geom_point() +
   scale_x_log10() +
   facet_wrap(~ continent)
+
+# Scatter plot comparing gdpPercap and lifeExp, with color representing continent
+# and size representing population, faceted by year
+
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp, color = continent, siz = pop)) +
+  geom_point() +
+  scale_x_log10() +
+  facet_wrap(~ year)
+
+# Filter for 1957 then summarize the median life expectancy
+gapminder %>%
+  filter(year== 1957) %>%
+  summarize(medianLifeExp= median(lifeExp))
+
+# Filter for 1957 then summarize the median life expectancy and the maximum GDP per capita
+
+gapminder %>%
+  filter(year== 1957) %>%
+  summarize(medianLifeExp= median(lifeExp), maxGdpPercap = max(gdpPercap))
+
+# Find median life expectancy and maximum GDP per capita in each year
+gapminder %>%
+  group_by(year) %>%
+  summarize(medianLifeExp = median(lifeExp), maxGdpPercap = max(gdpPercap))
+
+# Find median life expectancy and maximum GDP per capita in each continent in 1957
+gapminder %>%
+  filter(year == 1957) %>%
+  group_by(continent) %>%
+  summarize(medianLifeExp = median(lifeExp), maxGdpPercap = max(gdpPercap))
+
+# Find median life expectancy and maximum GDP per capita in each continent/year combination
+gapminder %>%
+  group_by(year, continent) %>%
+  summarize(medianLifeExp = median(lifeExp), maxGdpPercap = max(gdpPercap))
